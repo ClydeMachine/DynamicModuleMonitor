1f38d980b26eea37fb535d3a9f91db874ee62e39
Joe Greene <clydemachine@gmail.com>
2017-05-04 18:09:13 -0700

    Fix issue of incorrect logging path for Modules 01 and 02.

--- loadingdock/Module01.scala
+++ loadingdock/Module01.scala
@@ -8,7 +8,7 @@ class Module01 (
 }
 
 val module = new Module01()
-val filename_out = s"/var/log/${module.name}.log"
+val filename_out = s"var/log/${module.name}.log"
 val writer = new PrintWriter(new FileOutputStream(new File(filename_out), true))
 writer.write(s"[${val now = Calendar.getInstance().getTime}] ${module.name} initialized!\n")
 writer.close()
